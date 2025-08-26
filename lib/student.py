cca7bfc0d7bb54496735f7ed1bf6e9d82d31418d
Benadette Mokandu <benadettemokandu@gmail.com>
2025-08-26 04:58:18 +0300

    initial commit

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
-from user import User
+from user import User   # assuming your User class is in user.py
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []   # start with empty list
+
+    def learn(self, info):
+        self.knowledge.append(info)
--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
-
 from user import User
-
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        # give Teacher some knowledge (must not be empty)
+        self.knowledge = ["Math", "Science", "History"]
 
     def teach(self):
-        pass
+        # return a random item from knowledge
+        return random.choice(self.knowledge)
